959578701373a1458949d311ad7a8e06bab162e0
苏世龙 <flyingtimeice@163.com>
2019-11-09 09:27:39 +0800

    完成demo

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Demo for c++ QWidget embed into Qml 
+[参考网址 1](https://blog.csdn.net/r5014/article/details/92642626)
+[参考网址 2](https://code-examples.net/en/q/c6958f)
+[参考网址 3](https://blog.csdn.net/weixin_40912639/article/details/85256132)
+[参考网址 4](https://blog.csdn.net/r5014/article/details/72528418)
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <QApplication>
+#include <QPushButton>
+#include "QmlOSRExpMainWindow.h"
+#include "WidgetOSRItem.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QApplication::setAttribute(Qt::AA_UseSoftwareOpenGL, true);
+
+    //register diy qml tools
+    qmlRegisterType<WidgetOSRItem>("Diy.WidgetOSRItem",1,0, "WidgetOSRItem");
+
+    QmlOSRExpMainWindow w;
+    w.show();
+    w.load(QUrl("qrc:/WidgetOSRExp.qml"));
+
+
+    //新建一个QWebView放入qml osr item中
+    //QtWebView::initialize();
+
+    //新建一个QWebView放入qml osr item中
+//    QWebView osrWebView;
+//    osrWebView.setUrl(QUrl("http://www.baidu.com"));
+//    w.rootItem()->setProperty("osrItem_OSRWidget", QVariant::fromValue(&osrWebView));
+
+    QPushButton* btn = new QPushButton;
+    btn->setStyleSheet("background-color:red;");
+    btn->setAttribute(Qt::WA_NativeWindow);
+    w.rootItem()->setProperty("osrItem_OSRWidget", QVariant::fromValue(btn));
+
+    int res =  a.exec();
+    return res;
+}
